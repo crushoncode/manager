e1346856487a82ab89c15cc4705ff924f57d7a6a
Serina Ko <serinako08@gmail.com>
2018-10-17 10:50:13 +1100

    work on dynamic property updates

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Field, Button } from './common';
 
 class EmployeeCreate extends Component {
@@ -6,11 +8,25 @@ class EmployeeCreate extends Component {
     return (
       <Card>
         <CardSection>
-          <Field label="Name" placeholder="Jane" />
+          <Field
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={(value) =>
+              this.props.employeeUpdate({ prop: 'name', value })
+            }
+          />
         </CardSection>
 
         <CardSection>
-          <Field label="Phone" placeholder="555-555-5555" />
+          <Field
+            label="Phone"
+            placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={(value) =>
+              this.props.employeeUpdate({ prop: 'phone', value })
+            }
+          />
         </CardSection>
 
         <CardSection />
@@ -23,4 +39,12 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(
+  mapStateToProps,
+  { employeeUpdate }
+)(EmployeeCreate);
