130f7e465fef4b6cb67cc28bfb481425aca195f1
Serina Ko <serinako08@gmail.com>
2018-10-17 15:24:19 +1100

    fetch data from firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,10 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
+} from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -21,6 +25,22 @@ export const employeeCreate = ({ name, phone, shift }) => {
       .then(() => {
         dispatch({ type: EMPLOYEE_CREATE });
         Actions.pop();
+      });
+  };
+};
+
+// asyncronous actions
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+  return (dispatch) => {
+    firebase.database
+      .ref(`/users/${currentUser.uid}/employees`)
+      // snapshot: object that describes what data is in there
+      .on('value', (snapshot) => {
+        dispatch({
+          type: EMPLOYEES_FETCH_SUCCESS,
+          payload: snapshot.val()
+        });
       });
   };
 };
