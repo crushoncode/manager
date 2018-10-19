7fc078cb3fdc3b5f5d1d88b3ec8a1fcb93e6bc6f
Serina Ko <serinako08@gmail.com>
2018-10-19 15:21:12 +1100

    refactor codes using FlatList and fix lifecycle hook

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,46 +1,30 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView } from 'react-native';
+import { FlatList } from 'react-native';
+// import { ListView } from 'react-native';
 import { employeesFetch } from '../actions';
 import ListItem from './ListItem';
 
 class EmployeeList extends Component {
-  componentWillMount() {
+  componentDidMount() {
     // call an action creator (asynchronous request)
     // load employees
     this.props.employeesFetch();
-
-    this.createDataSource(this.props);
   }
 
-  componentWillReceiveProps(nextProps) {
-    // nextProps are the next set of props that this component will be rendered with
-    // this.props is still the old set of props
-
-    this.createDataSource(nextProps);
-  }
-
-  createDataSource({ employees }) {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 != r2
-    });
-
-    // To update the data in the datasource, use cloneWithRows.
-    this.dataSource = ds.cloneWithRows(employees);
-  }
-
-  renderRow(employee) {
-    return <ListItem employee={employee} />;
+  renderItem({ item }) {
+    return <ListItem employee={item} />;
   }
 
   render() {
     console.log(this.props);
     return (
-      <ListView
-        enableEmptySections
-        dataSource={this.dataSource}
-        renderRow={this.renderRow}
+      <FlatList
+        data={this.props.employees}
+        extraData={this.props.employees}
+        renderItem={this.renderItem}
+        keyExtractor={(employee) => employee.id}
       />
     );
   }
